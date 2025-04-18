cfa979a7c46cee87df2c5642b388fd1b8cb96a5a
Ruben Salazar <rubensalazar@rocketmail.com>
2025-04-18 17:38:47 -0500

    Teacher get in app.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from models import Student, Teacher, Appointment
+
 # Remote library imports
 from flask import request
 from flask_restful import Resource
@@ -9,19 +9,17 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import Student, Teacher, Appointment
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Music Shed</h1>'
+
 
 
 class StudentList(Resource):
     def get(self):
         students = Student.query.all()
-        return [student.to_dict() for student in students], 200
+        return [student.to_dict(rules=('-appointments', '-teachers')) for student in students], 200
 
     def post(self):
         data = request.get_json()
@@ -35,6 +33,28 @@ class StudentList(Resource):
         db.session.commit()
     
         return new_student.to_dict(), 201
+    
+    
+api.add_resource(StudentList, '/students')
+
+
+
+class TeacherList(Resource):
+    def get(self):
+        teachers = Teacher.query.all()
+        return [teacher.to_dict(rules=('-appointmetns', '-students')) for teacher in teachers], 200
+    
+    
+    # def post(self):
+    #     data = request.get_json()
+    #     new_teacher = Teacher(
+            
+    #     )
+    
+    
+@app.route('/')
+def index():
+    return '<h1>Music Shed</h1>'
     
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
