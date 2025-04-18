a5e902b412a54780f5729c505f223b244958d1c5
Ruben Salazar <rubensalazar@rocketmail.com>
2025-04-18 14:42:48 -0500

    new student form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import NewStudentForm from "./NewStudentForm"
+
+
 
 function App() {
-  return <h1>Project Client</h1>;
+  return <div>
+
+   <h1>🎵 Music Shed 🎵</h1>
+          <NewStudentForm/>
+        </div>
 }
 
 export default App;
