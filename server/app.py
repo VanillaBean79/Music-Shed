46c8313fb12e11e93493abc4c0fe68fba0e97993
Ruben Salazar <rubensalazar@rocketmail.com>
2025-04-18 13:57:32 -0500

    POST to students table

--- server/app.py
+++ server/app.py
@@ -24,7 +24,21 @@ def get_students():
 #   return [object.to_dict() for object in query_result], status_code
     return [student.to_dict() for student in students], 200
 
-
+@app.route('/students', methods=['POST'])
+def add_new_student():
+    data = request.get_json()
+    
+    new_student = Student(
+        name = data.get('name'),
+        age = data.get('age'),
+        instrument = data.get('instrument')
+    )
+    
+    db.session.add(new_student)
+    db.session.commit()
+    
+    return new_student.to_dict(), 201
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
